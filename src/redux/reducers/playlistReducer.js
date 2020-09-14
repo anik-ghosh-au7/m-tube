b5c6c5f633f0ffb0235c6aa71beb1e495e83700c
Anik Ghosh <tech.anikghosh@gmail.com>
2020-09-14 19:48:47 +0530

    second commit

--- src/redux/reducers/playlistReducer.js
+++ src/redux/reducers/playlistReducer.js
@@ -1,23 +1,25 @@
 const initialState = {
-    playlist:null,
-    pending_playlist:[]
+  playlist: null,
+  pending_playlist: [],
 };
 
 export default (state = initialState, action) => {
-    const {payload, type} = action;
-    
-    switch(type) {
-        case 'FETCH_PLAYLIST':
-            //important
-            return {...state, playlist: payload.items}
-        break;
+  const { payload, type } = action;
 
-        case 'FETCH_PLAYLIST_PENDING':
-            return {...state, pending_playlist: state.pending_playlist.concat(payload) }
-        
-        default:
-            break;
-    }
+  switch (type) {
+    case "FETCH_PLAYLIST":
+      //important
+      return { ...state, playlist: payload.items };
 
-    return state;
-}
+    case "FETCH_PLAYLIST_PENDING":
+      return {
+        ...state,
+        pending_playlist: state.pending_playlist.concat(payload),
+      };
+
+    default:
+      break;
+  }
+
+  return state;
+};
